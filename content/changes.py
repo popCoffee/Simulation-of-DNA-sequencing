cb548a45f17779144b5cd396230ecbc1b2309227
Andrew <36747017+popCoffee@users.noreply.github.com>
2018-05-28 21:38:31 -0700

    Create changes.py

---
+++ content/changes.py
@@ -0,0 +1,33 @@
+#python NN 
+
+
+def multilayer_perceptron(x, weights, biases):
+    '''
+    x : Place Holder for Data Input
+    weights: Dictionary of weights
+    biases: Dicitionary of biases
+    '''
+    
+    # First Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    
+    # Second Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    
+    # Last Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
